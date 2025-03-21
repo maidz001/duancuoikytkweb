f57fcc9d573ac4b72290c6d6e8210ee075a463e9
maidz <maipro8a4@gmail.com>
2025-03-21 23:26:40 +0700

    GVHHhcfdrtcfvghbjn

--- main5.js
+++ main5.js
@@ -5,3 +5,152 @@ function openMap(address) {
 if (localStorage.getItem("dangnhap") === "an") {
   document.getElementById("logg").style.display = "none";
 }
+function m18(nameks) {
+  localStorage.setItem("nameks", nameks);
+  window.location.href = "bookks.html";
+}
+function m19() {
+  if (
+    document.getElementById("gia").value === "tatca" &&
+    document.getElementById("dongtour").value === "tatca" &&
+    document.getElementById("danhgia").value === "tatca"
+  ) {
+    document.getElementById("ks1").style.display = "";
+    document.getElementById("ks2").style.display = "";
+    document.getElementById("ks3").style.display = "";
+    document.getElementById("ks4").style.display = "";
+    document.getElementById("ks5").style.display = "";
+    document.getElementById("ks6").style.display = "";
+    document.getElementById("ks7").style.display = "";
+    document.getElementById("ks8").style.display = "";
+  } else if (
+    document.getElementById("gia").value === "tren500" &&
+    (document.getElementById("dongtour").value === "caocap" ||
+      document.getElementById("dongtour").value === "tatca") &&
+    (document.getElementById("danhgia").value == "5" ||
+      document.getElementById("danhgia").value === "tatca")
+  ) {
+    document.getElementById("ks1").style.display = "none";
+    document.getElementById("ks2").style.display = "";
+    document.getElementById("ks3").style.display = "none";
+    document.getElementById("ks4").style.display = "";
+    document.getElementById("ks5").style.display = "none";
+    document.getElementById("ks6").style.display = "none";
+    document.getElementById("ks7").style.display = "none";
+    document.getElementById("ks8").style.display = "none";
+  } else if (
+    document.getElementById("gia").value === "duoi500" &&
+    document.getElementById("dongtour").value === "tatca" &&
+    document.getElementById("danhgia").value === "tatca"
+  ) {
+    document.getElementById("ks1").style.display = "";
+    document.getElementById("ks2").style.display = "none";
+    document.getElementById("ks3").style.display = "";
+    document.getElementById("ks4").style.display = "none";
+    document.getElementById("ks5").style.display = "";
+    document.getElementById("ks6").style.display = "";
+    document.getElementById("ks7").style.display = "";
+    document.getElementById("ks8").style.display = "";
+  } else if (
+    (document.getElementById("gia").value === "tren500" ||
+      document.getElementById("gia").value === "tatca") &&
+    document.getElementById("dongtour").value === "caocap" &&
+    (document.getElementById("danhgia").value == "5" ||
+      document.getElementById("danhgia").value === "tatca")
+  ) {
+    document.getElementById("ks1").style.display = "none";
+    document.getElementById("ks2").style.display = "";
+    document.getElementById("ks3").style.display = "none";
+    document.getElementById("ks4").style.display = "";
+    document.getElementById("ks5").style.display = "none";
+    document.getElementById("ks6").style.display = "none";
+    document.getElementById("ks7").style.display = "none";
+    document.getElementById("ks8").style.display = "none";
+  } else if (
+    (document.getElementById("gia").value === "duoi500" ||
+      document.getElementById("gia").value === "tatca") &&
+    document.getElementById("dongtour").value === "tieuchuan" &&
+    (document.getElementById("danhgia").value == "4" ||
+      document.getElementById("danhgia").value === "tatca")
+  ) {
+    document.getElementById("ks1").style.display = "";
+    document.getElementById("ks2").style.display = "none";
+    document.getElementById("ks3").style.display = "none";
+    document.getElementById("ks4").style.display = "none";
+    document.getElementById("ks5").style.display = "";
+    document.getElementById("ks6").style.display = "";
+    document.getElementById("ks7").style.display = "";
+    document.getElementById("ks8").style.display = "none";
+  } else if (
+    (document.getElementById("gia").value === "duoi500" ||
+      document.getElementById("gia").value === "tatca") &&
+    document.getElementById("dongtour").value === "tietkiem" &&
+    (document.getElementById("danhgia").value == "3" ||
+      document.getElementById("danhgia").value === "tatca")
+  ) {
+    document.getElementById("ks1").style.display = "none";
+    document.getElementById("ks2").style.display = "none";
+    document.getElementById("ks3").style.display = "";
+    document.getElementById("ks4").style.display = "none";
+    document.getElementById("ks5").style.display = "none";
+    document.getElementById("ks6").style.display = "none";
+    document.getElementById("ks7").style.display = "none";
+    document.getElementById("ks8").style.display = "";
+  } else if (
+    (document.getElementById("gia").value === "tren500" ||
+      document.getElementById("gia").value === "tatca") &&
+    (document.getElementById("dongtour").value === "caocap" ||
+      document.getElementById("dongtour").value === "tatca") &&
+    document.getElementById("danhgia").value == "5"
+  ) {
+    document.getElementById("ks1").style.display = "none";
+    document.getElementById("ks2").style.display = "";
+    document.getElementById("ks3").style.display = "none";
+    document.getElementById("ks4").style.display = "";
+    document.getElementById("ks5").style.display = "none";
+    document.getElementById("ks6").style.display = "none";
+    document.getElementById("ks7").style.display = "none";
+    document.getElementById("ks8").style.display = "none";
+  } else if (
+    (document.getElementById("gia").value === "duoi500" ||
+      document.getElementById("gia").value === "tatca") &&
+    (document.getElementById("dongtour").value === "tieuchuan" ||
+      document.getElementById("dongtour").value === "tatca") &&
+    document.getElementById("danhgia").value == "4"
+  ) {
+    document.getElementById("ks1").style.display = "";
+    document.getElementById("ks2").style.display = "none";
+    document.getElementById("ks3").style.display = "none";
+    document.getElementById("ks4").style.display = "none";
+    document.getElementById("ks5").style.display = "";
+    document.getElementById("ks6").style.display = "";
+    document.getElementById("ks7").style.display = "";
+    document.getElementById("ks8").style.display = "none";
+  } else if (
+    (document.getElementById("gia").value === "duoi500" ||
+      document.getElementById("gia").value === "tatca") &&
+    (document.getElementById("dongtour").value === "tietkiem" ||
+      document.getElementById("dongtour").value === "tatca") &&
+    document.getElementById("danhgia").value == "3"
+  ) {
+    document.getElementById("ks1").style.display = "none";
+    document.getElementById("ks2").style.display = "none";
+    document.getElementById("ks3").style.display = "";
+    document.getElementById("ks4").style.display = "none";
+    document.getElementById("ks5").style.display = "none";
+    document.getElementById("ks6").style.display = "none";
+    document.getElementById("ks7").style.display = "none";
+    document.getElementById("ks8").style.display = "";
+  }
+  // ------------------------
+  else {
+    document.getElementById("ks1").style.display = "none";
+    document.getElementById("ks2").style.display = "none";
+    document.getElementById("ks3").style.display = "none";
+    document.getElementById("ks4").style.display = "none";
+    document.getElementById("ks5").style.display = "none";
+    document.getElementById("ks6").style.display = "none";
+    document.getElementById("ks7").style.display = "none";
+    document.getElementById("ks8").style.display = "none";
+  }
+}
